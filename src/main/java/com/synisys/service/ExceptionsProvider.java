491f08394275cd2c199ea9dda4e3e617a6d776de
Garik Kalashyan <garegin.kalashyan@arm.synisys.com>
2017-02-15 18:31:08 +0400

    asdsad

---
+++ src/main/java/com/synisys/service/ExceptionsProvider.java
@@ -0,0 +1,10 @@
+package com.synisys.service;
+
+/**
+ * Created by Garegin.Kalashyan on 2/15/2017.
+ */
+public class ExceptionsProvider {
+    public static void run(){
+        System.out.println("asdasd");
+    }
+}
